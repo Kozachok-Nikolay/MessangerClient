3384447d203f2555f635f3797b0efd22738cab83
Николай Козачок <deraeser@bk.ru>
2021-11-15 23:45:31 +0300

    Gotovo

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt6 import uic, QtCore, QtGui, QtWidgets
+import datetime
+import requests
+from requests.exceptions import HTTPError
+import json
 
 
-# Press the green button in the gutter to run the script.
+class MainWindow(QtWidgets.QMainWindow):
+    ServerAdress = "http://localhost:5000"
+    MessageID = 0
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('Client.ui', self)
+        self.pushButton1.clicked.connect(self.pushButton1_clicked)
+
+    def pushButton1_clicked(self):
+        self.SendMessage()
+
+    def SendMessage(self):
+        UserName = self.lineEdit1.text()
+        MessageText = self.lineEdit2.text()
+        TimeStamp = str(datetime.datetime.today())
+        msg = f"{{\"UserName\": \"{UserName}\", \"MessageText\": \"{MessageText}\", \"TimeStamp\": \"{TimeStamp}\"}}"
+        # {"UserName": "RusAl", "MessageText": "Privet na sto let!!!", "TimeStamp": "2021-03-05T18:23:10.932973Z"}
+        print("Отправлено сообщение: " + msg)
+        url = self.ServerAdress + "/api/mes"
+        data = json.loads(msg)  # string to json
+        r = requests.post(url, json=data)
+        # print(r.status_code, r.reason)
+
+    def GetMessage(self, id):
+        url = self.ServerAdress + "/api/mes/" + str(id)
+        # print(url)
+        try:
+            response = requests.get(url)
+            # если ответ успешен, исключения задействованы не будут
+            response.raise_for_status()
+        except HTTPError as http_err:
+            # print(f'HTTP error occurred: {http_err}')  # Python 3.6
+            return None
+        except Exception as err:
+            # print(f'Other error occurred: {err}')  # Python 3.6
+            return None
+        else:
+            text = response.text
+            return text
+
+    def timerEvent(self):
+        msg = self.GetMessage(self.MessageID)
+        while msg is not None:
+            msg = json.loads(msg)
+            UserName = msg["UserName"]
+            MessageText = msg["MessageText"]
+            TimeStamp = msg["TimeStamp"]
+            msgtext = f"{TimeStamp} : <{UserName}> : {MessageText}"
+            print(msgtext)
+            self.listWidget1.insertItem(self.MessageID, msgtext)
+            self.MessageID += 1
+            msg = self.GetMessage(self.MessageID)
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QtWidgets.QApplication(sys.argv)
+    w = MainWindow()
+    w.show()
+    timer = QtCore.QTimer()
+    time = QtCore.QTime(0, 0, 0)
+    timer.timeout.connect(w.timerEvent)
+    timer.start(5000)
+    sys.exit(app.exec())
